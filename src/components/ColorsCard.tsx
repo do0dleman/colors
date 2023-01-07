554d4065dfbdc5a3de85399a962b35373329adae
do0dleman <97869897+do0dleman@users.noreply.github.com>
2023-01-07 17:45:02 +0200

    hex copying on click added

--- src/components/ColorsCard.tsx
+++ src/components/ColorsCard.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react"
 import HSVtoHSL from "../functions/HSVtoHSL";
 import RGBtoHEX from "../functions/RGBtoHEX";
 import RGBtoString from "../functions/RGBtoString";
+import copy from "copy-to-clipboard";
 
 interface IColorCardProps {
     colorRGB: [number, number, number]
@@ -13,6 +14,7 @@ export default function ColorsCard(props: IColorCardProps) {
     const [hex, setHex] = useState<String>(RGBtoHEX(props.colorRGB))
     const [colorName, setColorName] = useState<String>('')
     const [colorsText, setColorsText] = useState<string>('colors__code')
+    const [showHexNot, setShowHexNot] = useState<boolean>(false)
 
     useEffect(() => {
         card.current!.style.backgroundColor = RGBtoString(props.colorRGB);
@@ -35,13 +37,26 @@ export default function ColorsCard(props: IColorCardProps) {
                 setColorName(data.paletteTitle)
             })
     }, [])
+
+    function copyHexToClipboard() {
+        copy(`#${hex}`)
+        setShowHexNot(true)
+        setTimeout(() => {
+            setShowHexNot(false)
+        }, 2500)
+    }
     return (
         <div ref={card} className="colors__card">
-            <p className={colorsText}>{`#${hex}`}</p>
+            <p className={colorsText} onClick={copyHexToClipboard}>{`#${hex}`}</p>
             <p className={colorsText}>{colorName}</p>
             {/* <p className={colorsText}>{`hsv(${Math.round(props.colorHSV[0] * 100) / 100}, 
             ${Math.round(props.colorHSV[1] * 100) / 100}, 
             ${Math.round(props.colorHSV[2] * 100) / 100})`}</p> */}
+            <div className={
+                `colors__hex-notification ${showHexNot ?
+                    'colors__hex-notification-active' : 'colors__hex-notification-inactive'}`
+            }
+            >{`#${hex} has been copied`}</div>
         </div>
     )
 }
